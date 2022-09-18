8169f78db98564efe15d3501e7a94231cbbe2d58
nhyde2000-1 <nolanhyde2000@gmail.com>
2022-09-18 18:16:41 +0000

    Sep 18, 2022, 2:16 PM

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -51,8 +51,8 @@ public class PlayerController : MonoBehaviour
         }
         if (count == 8)
         {
-            transform.position = new Vector3(0.0f, 0.0f, 47.74.0f);
-        } 
+            transform.position = new Vector3(0.0f, 0.0f, 47.74f);
+        }
     }
 
     void OnMove(InputValue value)
